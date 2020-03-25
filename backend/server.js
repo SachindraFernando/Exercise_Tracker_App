b4f4ffdf011433c4b3ed9c11d40cbf08b4e42781
Sachindra Fernando <sachindrafernando3@gmail.com>
2020-03-25 12:08:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT||5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true});
@@ -20,8 +21,10 @@ connection.once('open',()=>{
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
-app.use('exercises', exercisesRouter);
-app.use('users', usersRouter);
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+
 
 app.listen(port,()=>{
 console.log(`Server is running on port: ${port}`);
